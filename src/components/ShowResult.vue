85df6e4b5067bac3f51da9ab42ee7b3bde20c29a
mislamfullstackdev <mislam.pasona@gmail.com>
2024-12-09 09:13:05 +0900

    Added rank

--- src/components/ShowResult.vue
+++ src/components/ShowResult.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="result">
     <h2>Your reaction score is : {{ score }} </h2>
+    <p class="rank">{{ rank }}</p>
   </div>
 </template>
 
 <script>
 export default {
     props: ['score'],
+    data(){
+      return{
+        rank: null
+      }
+    },
+    mounted(){
+      if( this.score < 200){
+        this.rank = 'Ninja Fingers'
+      } else if(this.score < 400){
+        this.rank = 'Rapid Fingers'
+      }else{
+        this.rank = 'snail pace'
+      }
+    }
 }
 </script>
 
@@ -19,5 +34,11 @@ export default {
         text-align: center;
         padding: 100px 0px;
         margin: 4px auto;
+    }
+    .rank{
+      font-size: 1.4rem;
+      color: #0faf87;
+      font-weight: ;
+
     }
 </style>
